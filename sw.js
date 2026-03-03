925c54e0617bce0fb29c22f3e04a195cb82d66b4
losha24 <losha24@gmail.com>
2026-03-03 13:20:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,55 @@
-const CACHE_NAME = "gogo-cache-v1.1.5";
+const CACHE_NAME = "gogo-v1.1.6";
+
 const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './soccer_icon_192.png',
-  './soccer_logo.png'
+"/",
+"/index.html",
+"/manifest.json"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache => cache.addAll(urlsToCache))
+
+);
+
+self.skipWaiting();
+
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+
+caches.keys().then(keys =>
+
+Promise.all(
+
+keys.filter(key => key !== CACHE_NAME)
+
+.map(key => caches.delete(key))
+
+)
+
+)
+
+);
+
+self.clients.claim();
+
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
-  );
+self.addEventListener("fetch", event => {
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response => response || fetch(event.request))
+
+);
+
 });
