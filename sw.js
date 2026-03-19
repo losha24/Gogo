e292922cb791851d526f405b58e4ac6bc773ec52
losha24 <losha24@gmail.com>
2026-03-19 10:18:17 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'okar-v2.2.0';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
