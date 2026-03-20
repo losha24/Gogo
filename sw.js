092fca70b2522f7e90faa24e47bcf5e5e8b57983
losha24 <losha24@gmail.com>
2026-03-20 20:24:06 +0200

    Update sw.js
    533

--- sw.js
+++ sw.js
@@ -1,45 +1,14 @@
-const CACHE_NAME = 'okar-v5.3.2';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './manifest.json',
-  './logo.png'
-];
+const CACHE_NAME = 'okar-v5.3.3';
+const ASSETS = ['./', './index.html', './style.css', './manifest.json', './logo.png'];
 
-// התקנה ושמירת קבצים בזיכרון
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching assets...');
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// ניקוי גרסאות ישנות מהזיכרון של הטלפון
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            console.log('Removing old cache:', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); }))));
   return self.clients.claim();
 });
 
-// ניהול בקשות רשת
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
