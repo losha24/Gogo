087ec5b7cea599112ff71dd80a8d302acbb91c4a
losha24 <losha24@gmail.com>
2026-03-20 13:59:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'okar-v5.2.0';
+const CACHE_NAME = 'okar-v5.3.0';
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './logo.png'];
 
 self.addEventListener('install', e => {
